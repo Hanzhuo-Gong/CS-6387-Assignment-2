1e9b62a797736dba72067312ea875c61a5314bc4
Hanzhuo-Gong <harvardgong@hotmail.com>
2024-06-01 21:49:07 -0700

    updated code

--- public/home.js
+++ public/home.js
@@ -1,4 +1,3 @@
-
 const loginButton = document.getElementById("login-btn");
 const registerButton = document.getElementById("register-btn");
 const registerMessage = document.getElementById("register-message");
@@ -8,8 +7,16 @@ const registerUsernameInput = document.getElementById("register-username");
 const registerPasswordInput = document.getElementById("register-password");
 const loginUsernameInput = document.getElementById("login-username")
 const loginPasswordInput = document.getElementById("login-password")
+
+const bypassInjectionButton = document.getElementById("bypassing-injection-btn");
+const blindInjectionButton = document.getElementById("blind-injection-btn")
+const errorbasedInjectionButton = document.getElementById("error-injection-btn")
+
 loginButton.addEventListener("click", handleLogin);
 registerButton.addEventListener("click", handleRegister);
+bypassInjectionButton.addEventListener("click", handleBypassInjection);
+blindInjectionButton.addEventListener("click", handleBlindInjection);
+errorbasedInjectionButton.addEventListener("click", handleErrorbasedInjection);
 
 registerUsernameInput.addEventListener("input", hideRegisterMessage);
 registerPasswordInput.addEventListener("input", hideRegisterMessage);
@@ -28,9 +35,7 @@ function handleLogin() {
     const username = document.getElementById("login-username").value;
     const password = document.getElementById("login-password").value;
 
-    console.log("Login clicked")
-
-    fetch('https://localhost:3000/login', {
+    fetch('http://localhost:3000/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -48,7 +53,6 @@ function handleLogin() {
         .then(data => {
             document.getElementById("login-message").textContent = data.message;
             loginMessage.style.visibility = "visible";
-            console.log("Login succeeded");
         })
         .catch(error => {
             document.getElementById("login-message").textContent = error.message;
@@ -61,9 +65,7 @@ function handleRegister() {
     const username = document.getElementById("register-username").value;
     const password = document.getElementById("register-password").value;
 
-    console.log("Register clicked");
-
-    fetch('https://localhost:3000/register', {
+    fetch('http://localhost:3000/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -81,7 +83,7 @@ function handleRegister() {
         .then(data => {
             document.getElementById("register-message").textContent = data.message;
             registerMessage.style.visibility = "visible";
-            console.log("Register succeeded");
+            //console.log("Register successfully");
         })
         .catch(error => {
             document.getElementById("register-message").textContent = error.message;
@@ -90,4 +92,61 @@ function handleRegister() {
         });
 }
 
+// Sample Bypass Injection Attack
+function handleBypassInjection() {
+    const username = "' OR '1'='1";
+    const password = "anything";
+    const type = "bypass"
 
+    performLogin(username, password, type)
+}
+
+// Sample Blind Injection Attack
+function handleBlindInjection() {
+    //const username = "1' OR 1=1 --";
+    const username = "admin"
+    const password = "anything";
+    const type = "blind"
+
+    performLogin(username, password, type);
+}
+
+// Sample Error-based Injection Attack
+function handleErrorbasedInjection() {
+    const username = "admin";
+    const password = "anything";
+    const type = "error-based";
+
+    performLogin(username, password, type);
+}
+
+// Help method to perform login
+function performLogin(username, password, type) {
+    fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password, type })
+    })
+        .then(response => {
+            console.log('Response status:', response.status);
+            if (!response.ok) {
+                return response.json().then(data => {
+                    console.log('Response data:', data);
+                    throw new Error(data.message);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Successful response:', data);
+            document.getElementById("testing-message").textContent = data.message;
+            document.getElementById("testing-message").style.visibility = "visible";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById("testing-message").textContent = error.message;
+            document.getElementById("testing-message").style.visibility = "visible";
+        });
+}
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const bcrypt = require('bcrypt');
+
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Ensure your HTML, JS, and CSS are in the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Database setup
 let db = new sqlite3.Database(':memory:', (err) => {
@@ -18,7 +22,7 @@ let db = new sqlite3.Database(':memory:', (err) => {
     console.log('Connected to the in-memory SQLite database.');
 });
 
-// Create a table
+// Create a table for sql
 db.run('CREATE TABLE users(username TEXT, password TEXT)', (err) => {
     if (err) {
         console.error(err.message);
@@ -27,44 +31,166 @@ db.run('CREATE TABLE users(username TEXT, password TEXT)', (err) => {
 
 // API endpoint to handle login
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, row) => {
+    const { username, password, type } = req.body;
+
+    // Validate input, to make sure the user enter both username and password.
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    }
+
+    // Sanitize input, to ensure all user inputs are properly sanitized and validated before processing or storing them in the database.
+    // This is using regex to make sure the username only contains lowercase letter, uppercase letter and digits.
+    // Sanitizing the username ensures that it contains only valid characters and helps to prevent injection attacks.
+    // The ^ inside the square brackets [] means "not".
+    // a-zA-Z0-9 includes all lowercase letters (a-z), uppercase letters (A-Z), and digits (0-9).
+    // g (global flag): This means the pattern should be applied globally across the entire string, not just the first occurrence.
+    const sanitizedUsername = username.replace(/[^a-zA-Z0-9]/g, '');
+
+    // The ? in the query is a placeholder for the sanitizedUsername. 
+    // Using parameterized queries helps prevent SQL injection attacks by ensuring that the input is treated as a parameter rather than executable code.
+    db.get('SELECT * FROM users WHERE username = ?', [sanitizedUsername], (err, row) => {
+
+        // Handling Errors: if server side encountered error, sent the error to user
         if (err) {
             return res.status(500).json({ error: err.message });
         }
         if (row) {
+            // Compare the hashed password with the stored hashed password
+            // Default method: bcrypt.compare(plainTextPassword, hashedPassword, callback)
+            // bcrypt hashes the plain text password using the same salt and algorithm as the stored hash, and compare the stored hash value
+            bcrypt.compare(password, row.password, (err, result) => {
+                // Log in successfully, return the message
+                if (result) {
+                    res.json({ message: 'Login successful' });
+                }
+                // Handling Errors: Invalid credentials. The result will return either true or false.
+                // True if provided password is correct, and the user is authenticated. Else it will return false
+                else {
+                    res.status(401).json({ message: 'Invalid credentials' });
+                }
+            });
+        }
+        // Handling Errors: username does not existed, return the error message
+        else {
+            res.status(401).json({ message: 'Username does not exist' });
+        }
+    });
+
+
+    /*--------------------------------------BELOW ARE FOR INJECTION ATTACKS----------------------------------------------- */
+
+    /*
+    // Vulnerable query (for testing SQL injection) 
+    // Below Code will make the injection attack successful, just for testing purpose.
+    // Please commend the db.get() above to test this
+    let query = "";
+
+    // Bypassing Authentication: Vulnerable query (for testing SQL injection) 
+    // This will bypass the authentication process and directly get the data.
+    if (type === "bypass") {
+        query = `SELECT * FROM users WHERE username = '${username}' OR '1'='1'`;
+    }
+
+    // Blind Authentication: Please make sure to REGISTER the admin first, since it is targeting the admin.
+    // This type relies on asking the database true or false questions and determining the answer based on the application's response.
+    if (type === "blind") {
+        query = `SELECT * FROM users WHERE username = '${username}' AND 1=1`;
+    }
+
+    console.log(`Constructed SQL query: ${query}`);
+    // Execute the constructed SQL query against the database
+    db.get(query, (err, row) => {
+        // Handling Errors: if server side encountered error, sent the error to user
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        console.log(`Query result: ${JSON.stringify(row)}`);
+        // Log in successfully, return the message
+        if (row) {
             res.json({ message: 'Login successful' });
-        } else {
+        }
+        // Handling Errors: Invalid credentials. The result will return either true or false.
+        else {
             res.status(401).json({ message: 'Invalid credentials' });
         }
     });
+    */
+
+
 });
 
 // API endpoint to handle registration
 app.post('/register', (req, res) => {
     const { username, password } = req.body;
 
-    // Check if the username already exists
-    db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+    // Validate input, to make sure the user enter both username and password.
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required' });
+    }
+
+    // Sanitize input, to ensure all user inputs are properly sanitized and validated before processing or storing them in the database.
+    // This is using regex to make sure the username only contains lowercase letter, uppercase letter and digits. 
+    // Sanitizing the username ensures that it contains only valid characters and helps to prevent injection attacks.
+    // The ^ inside the square brackets [] means "not".
+    // a-zA-Z0-9 includes all lowercase letters (a-z), uppercase letters (A-Z), and digits (0-9).
+    // g (global flag): This means the pattern should be applied globally across the entire string, not just the first occurrence.
+    const sanitizedUsername = username.replace(/[^a-zA-Z0-9]/g, '');
+
+
+    // The ? in the query is a placeholder for the sanitizedUsername. 
+    // Using parameterized queries helps prevent SQL injection attacks by ensuring that the input is treated as a parameter rather than executable code. 
+    db.get('SELECT * FROM users WHERE username = ?', [sanitizedUsername], (err, row) => {
+
+        // Handling Errors: if server side encountered error, sent the error to user
         if (err) {
             return res.status(500).json({ error: err.message });
         }
+
+        // Handling Errors: Username already exists, existed username is prohibited.
         if (row) {
-            // Username already exists
             return res.status(409).json({ message: 'Username already exists' });
-        } else {
-            // Insert the new user
-            db.run('INSERT INTO users(username, password) VALUES(?, ?)', [username, password], function (err) {
+        }
+        // Passwords are typically hashed before storing in the database for security reasons. 
+        // When a user attempts to log in, their entered password is hashed and compared to the stored hashed password.
+        else {
+
+            // Hash the password. the password is hashed using bcrypt.
+            // This function hashes the password with a salt factor of 10. The salt factor determines the computational cost of hashing; 
+            // Higher values increase security but require more processing time.
+            bcrypt.hash(password, 10, (err, hashedPassword) => {
+
+                // Handling Errors: if server side encountered error, sent the error to user
                 if (err) {
                     return res.status(500).json({ error: err.message });
                 }
-                res.json({ message: 'User added successfully' });
+                // Inserted into the users table with the sanitizedUsername and the hashedPassword.
+                db.run('INSERT INTO users(username, password) VALUES(?, ?)', [sanitizedUsername, hashedPassword], function (err) {
+                    // Handling Errors: if server side encountered error, sent the error to user
+                    if (err) {
+                        return res.status(500).json({ error: err.message });
+                    }
+
+                    // Register successfully, sent the response message back to user
+                    res.json({ message: 'User added successfully' });
+                });
             });
         }
     });
+
 });
 
+
+app.get('/dump', (req, res) => {
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ users: rows });
+    });
+});
 /*
+// Below is to start the server as https
+
 // Read SSL certificate and key files
 const sslOptions = {
     key: fs.readFileSync('key.pem'),
@@ -77,6 +203,7 @@ https.createServer(sslOptions, app).listen(port, () => {
 });
 */
 
+// start the server as http
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
